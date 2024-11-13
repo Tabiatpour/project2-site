ae7c7cfe4fd411a732c799459bc5317f9603d3b8
STP <stabiatpour@gmail.com>
2024-11-13 15:57:23 +0330

    first-commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Saeedeh
         </a>
       </header>
     </div>
